c942ed36a09490aa0ea94e28021afdb7581eed16
Kamal-Aepuru <kamalaepuru@gmail.com>
2025-01-10 19:26:42 +0530

    updated mongo connection

--- api/index.js
+++ api/index.js
@@ -10,13 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error(err));
-
+    .catch((err) => console.error('MongoDB connection error:', err));
+    
 app.use('/api/users', require('../routes/userRoutes'));
 app.use('/api/posts', require('../routes/postRoutes'));
 
