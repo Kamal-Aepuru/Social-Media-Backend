3ee3a40c6865aa71af153c0850eb7692286bd566
Kamal-Aepuru <kamalaepuru@gmail.com>
2025-01-11 15:03:14 +0530

    routes updated

--- api/index.js
+++ api/index.js
@@ -14,8 +14,17 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
     
-app.use('/api/users', require('../routes/userRoutes'));
-app.use('/api/posts', require('../routes/postRoutes'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+
+// Default route for undefined endpoints
+// Default route for undefined endpoints
+app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Endpoint not found' });
+});
 
 module.exports = app;
 
